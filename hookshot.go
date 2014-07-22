7ea026c7b0c4306b9ce9256334a78fef1df03d9a
Eric J. Holmes <eric@ejholmes.net>
2014-07-22 08:42:47 -0700

    WIP.

---
+++ hookshot.go
@@ -0,0 +1,79 @@
+package hookshot
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	// HeaderEvent is the name of the header that contains the type of event.
+	HeaderEvent = "X-GitHub-Event"
+
+	// HeaderSignature is the name of the header that contains the signature.
+	HeaderSignature = "X-Hub-Signature"
+)
+
+// Router demultiplexes github hooks.
+type Router struct {
+	NotFoundHandler http.Handler
+	routes          routes
+}
+
+// NewRouter returns a new Router.
+func NewRouter() *Router {
+	return &Router{
+		routes:          make(routes),
+		NotFoundHandler: http.HandlerFunc(http.NotFound),
+	}
+}
+
+// Handle maps a github event to an http.Handler.
+func (rr *Router) Handle(event string, h http.Handler) {
+	route := &route{event: event, handler: h}
+	rr.routes[event] = route
+}
+
+// ServeHTTP implements the http.Handler interface.
+func (rr *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	event := r.Header.Get(HeaderEvent)
+
+	route := rr.routes[event]
+	if route == nil {
+		rr.NotFoundHandler.ServeHTTP(w, r)
+		return
+	}
+
+	if !authorized(r, route.secret) {
+		http.Error(w, "The provided signature in the "+HeaderSignature+" header does not match.", 403)
+	}
+}
+
+// route represents the http.Handler for a github event.
+type route struct {
+	secret  string
+	event   string
+	handler http.Handler
+}
+
+// routes maps a github event to a route.
+type routes map[string]*route
+
+// Signature calculates the SHA1 HMAC signature of in using the secret.
+func Signature(in []byte, secret string) string {
+	mac := hmac.New(sha1.New, []byte(secret))
+	mac.Write(in)
+	return fmt.Sprintf("%x", mac.Sum(nil))
+}
+
+// authorized checks that the calculated signature for the request matches the provided signature in
+// the request headers.
+func authorized(r *http.Request, secret string) bool {
+	raw, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return false
+	}
+	return r.Header.Get(HeaderSignature) == "sha1"+Signature(raw, secret)
+}
---
+++ hookshot_test.go
@@ -0,0 +1,78 @@
+package hookshot
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_Router(t *testing.T) {
+	router := NewRouter()
+
+	router.Handle("deployment", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+
+	tests := []struct {
+		event     string
+		body      string
+		signature string
+
+		status int
+	}{
+		{
+			event:     "",
+			body:      `{"event":"data"}`,
+			signature: "invalid",
+			status:    404,
+		},
+		{
+			event:     "foobar",
+			body:      `{"event":"data"}`,
+			signature: "invalid",
+			status:    404,
+		},
+		{
+			event:     "deployment",
+			body:      `{"event":"data"}`,
+			signature: "invalid",
+			status:    403,
+		},
+	}
+
+	for i, tt := range tests {
+		resp := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/", bytes.NewReader([]byte(tt.body)))
+		req.Header.Set("X-GitHub-Event", tt.event)
+
+		router.ServeHTTP(resp, req)
+
+		if resp.Code != tt.status {
+			t.Errorf("Code %v: Want %v; Got %v", i, tt.status, resp.Code)
+		}
+	}
+}
+
+func Test_Signature(t *testing.T) {
+	tests := []struct {
+		in     string
+		secret string
+
+		signature string
+	}{
+		{
+			`{"event":"data"}`,
+			"1234",
+			"ade133892a181fba3a21c163cd5cbc3f5f8e915c",
+		},
+	}
+
+	for i, tt := range tests {
+		signature := Signature([]byte(tt.in), tt.secret)
+
+		if signature != tt.signature {
+			t.Errorf("%v: Want %v; Got %v", i, tt.signature, signature)
+		}
+	}
+}
