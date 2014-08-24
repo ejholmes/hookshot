0bec243053597cffee10caf80d906aeda69dc0fe
Eric Holmes <eric@ejholmes.net>
2014-08-24 01:06:22 +0000

    copyright

--- hookshot.go
+++ hookshot.go
@@ -1,3 +1,7 @@
+// Copyright 2014 Eric Holmes.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package hookshot is a router that de-multiplexes and authorizes github webhooks.
 package hookshot
 
